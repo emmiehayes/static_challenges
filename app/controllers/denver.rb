1973de0e2597cd633666ae21b91df9c83a26dafc
Emmie Hayes <emmiehayes2@gmail.com>
2018-06-24 11:40:22 -0600

    test rackup populates gray page with testing text

--- app/controllers/denver.rb
+++ app/controllers/denver.rb
@@ -3,8 +3,8 @@ require 'rack'
 class Denver
   def self.call(env)
     case env["PATH_INFO"]
-    when '/'         then index.html
-    when '/index'    then about
+    when '/'         then index
+    when '/index'    then index
     when '/main.css' then css
     else
       error
@@ -21,5 +21,13 @@ class Denver
 
   def self.render_view(page, code = '200')
   [code, {'Content-Type' => 'text/html'}, [File.read("./app/views/#{page}")]]
+  end
+
+    def self.css
+    render_static('main.css')
+  end
+
+  def self.render_static(asset)
+    [200, {'Content-Type' => 'text/html'}, [File.read("./public/#{asset}")]]
   end
 end
